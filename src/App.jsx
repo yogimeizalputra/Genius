23d8eb99638612898e3db23e90b6b93e36519336
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-11-16 18:27:21 +0700

    Snap-scroll Effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Topbar from './components/topbar/Topbar';
+import Intro from './components/intro/Intro';
+import Portofolio from './components/portofolio/Portofolio';
+import Works from './components/works/Works';
+import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
+import './app.scss'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Topbar/>
       <div className="sections">
-        
+        <Intro/>
+        <Portofolio/>
+        <Works/>
+        <Testimonials/>
+        <Contact/>
       </div>
     </div>
   );
