fe011ed3b6d909d83759a4028ef0a65c5df7ec1f
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-11-19 20:01:06 +0700

    Material-Ui Icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './topbar.scss'
+import { Person, Mail } from '@material-ui/icons'
 
 const Topbar = () => {
     return (
@@ -8,7 +9,12 @@ const Topbar = () => {
                 <div className="left">
                     <a href="#intro" className="logo">genius.</a>
                     <div className="itemContainer">
-                        
+                        <Person className="icon" />
+                        <span>+44 924 12 74</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>info@genius.com</span>
                     </div>
                 </div>
                 <div className="right">
