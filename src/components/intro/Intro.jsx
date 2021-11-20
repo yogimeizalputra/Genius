27325e08a7ec5b8122befc131857cb3382576b9b
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-11-20 13:55:35 +0700

    Introduction Page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,23 @@ import './intro.scss'
 const Intro = () => {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Genius. Mcarty</h1>
+                    <h3>
+                        Freelance <span></span>
+                    </h3>
+                </div>
+                <a href="#portofolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
