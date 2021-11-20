9b3a61825151420fccd2015bdbbe948581d749c7
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-11-20 13:13:24 +0700

    Sidebar Menu

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Works from './components/works/Works';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import './app.scss'
+import Menu from './components/menu/Menu';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro/>
         <Portofolio/>
